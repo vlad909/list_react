a042449b897805e8000bd0c1b181f8a1cef659c2
Vladislav Gushchin <vladislav.g@cronix.ms>
2018-07-04 18:15:35 +0300

    divide like a component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import ReactDOM from 'react-dom'
 import './App.css';
 import EventEmitter from 'event-emitter'
+import Add from "./components/AddItem";
 
 class App extends Component {
     render() {
@@ -12,8 +12,8 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo"/>
                 </header>
                 <div className="side">
-                    <Add></Add>
-                    <ListOfTasks list={list}></ListOfTasks>
+                    <Add />
+                    <ListOfTasks list={list}/>
                 </div>
             </div>
         );
@@ -90,47 +90,6 @@ class ListOfTasks extends React.Component {
     }
 }
 
-class Add extends React.Component {
-    constructor(props) {
-        super(props)
-        // this.addButton = this.addButton.bind(this)
-        // this.rulesAccepter = this.rulesAccepter.bind(this)
-        this.state = {
-            agree: false
-        }
-    }
 
-    componentDidMount() {
-        ReactDOM.findDOMNode(this.refs.author).focus()
-    }
-
-    addButton() {
-        let author = ReactDOM.findDOMNode(this.refs.author).value, book = ReactDOM.findDOMNode(this.refs.book).value
-        let item = [{
-            author: author,
-            book: book
-        }]
-        window.ee.emit('List.add', item)
-    }
-
-    rulesAccepter(e) {
-        this.setState({agree: e.target.checked})
-    }
-
-    render() {
-        return (
-            <div>
-                <label>adding form</label>
-                <input className="test-input" type="text" defaultValue=""
-                       placeholder='author' ref="author"/>
-                <input type="text" ref="book" placeholder="book" defaultValue=""/>
-                <label htmlFor="">
-                    <input onChange={(e)=>this.rulesAccepter(e)} type="checkbox" defaultChecked={false} ref="checkrule"/>
-                </label>
-                <button disabled={!this.state.agree} onClick={() => this.addButton()}>value</button>
-            </div>
-        )
-    }
-}
 
 export default App;
