769443a81949e8b84dde0e164fd63f522e22059a
Vladislav Gushchin <vladislav.g@cronix.ms>
2018-07-04 16:23:02 +0300

    adding books

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
+import ReactDOM from 'react-dom'
 import './App.css';
+import EventEmitter from 'event-emitter'
 
 class App extends Component {
     render() {
@@ -10,12 +12,15 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo"/>
                 </header>
                 <div className="side">
+                    <Add></Add>
                     <ListOfTasks list={list}></ListOfTasks>
                 </div>
             </div>
         );
     }
 }
+
+window.ee = new EventEmitter()
 
 let list = [
     {
@@ -33,27 +38,37 @@ let list = [
 ]
 
 class ListOfTasks extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            value: true
+            value: true,
+            data: []
         }
     }
+
+    componentDidMount() {
+        this.setState({data: list})
+        window.ee.on('List.add', (item) => {
+        let added = item.concat(this.state.data)
+            this.setState({data: added})
+        })
+    }
+
     render() {
-        let data = this.props.list, newTemplate
-        if (data.length > 0) {
-            newTemplate = data.map((item, index) => {
+        let  newTemplate
+        if (this.state.data.length > 0) {
+            newTemplate = this.state.data.map((item, index) => {
                 return (
                     <li key={index}>{item.author}: {item.book}</li>
                 )
             })
         } else {
-            newTemplate = <strong> data is empty</strong>
+            newTemplate = <strong> this.state.data is empty</strong>
         }
         return (
             <div className="ListOfTasks">
                 <p>table</p>
-                <button onClick={()=>{
+                <button onClick={() => {
                     let current = this.state.value
                     console.log(current, this.state.value)
                     this.setState({
@@ -65,7 +80,42 @@ class ListOfTasks extends React.Component {
                 <ul className="table">
                     {newTemplate}
                 </ul>
-                <p className={this.state.value ? '': 'empty'}>General count: {data.length}</p>
+                <p className={this.state.value ? '' : 'empty'}>General count: {this.state.data.length}</p>
+            </div>
+        )
+    }
+}
+
+class Add extends React.Component {
+    constructor(props) {
+        super(props)
+        this.addButton = this.addButton.bind(this)
+    }
+
+    componentDidMount() {
+        ReactDOM.findDOMNode(this.refs.author).focus()
+    }
+
+    addButton() {
+        let author = ReactDOM.findDOMNode(this.refs.author).value, book = ReactDOM.findDOMNode(this.refs.book).value
+        let item = [{
+            author: author,
+            book: book
+        }]
+        window.ee.emit('List.add', item)
+    }
+
+    render() {
+        return (
+            <div>
+                <label>adding form</label>
+                <input className="test-input" type="text" defaultValue=""
+                       placeholder='author' ref="author"/>
+                <input type="text" ref="book" placeholder="book" defaultValue=""/>
+                <label htmlFor="">
+                <input type="checkbox" defaultChecked={false} ref="checkrule"/>
+                </label>
+                <button onClick={this.addButton}>value</button>
             </div>
         )
     }
